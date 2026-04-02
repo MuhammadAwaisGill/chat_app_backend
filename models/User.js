f7eb4a9e0f96c245747de5b4b62482986554c127
Muhammad Awais <muhammadawaisgill18@gmail.com>
2026-04-02 07:33:34 +0500

    feat: MongoDB Atlas connected, auth routes working

--- models/User.js
+++ models/User.js
@@ -1,51 +1,23 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema(
   {
-    name: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    avatarUrl: {
-      type: String,
-      default: null,
-    },
-    status: {
-      type: String,
-      enum: ['online', 'offline'],
-      default: 'offline',
-    },
-    lastSeen: {
-      type: Date,
-      default: Date.now,
-    },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    avatarUrl: { type: String, default: null },
+    status: { type: String, enum: ['online', 'offline'], default: 'offline' },
+    lastSeen: { type: Date, default: Date.now },
   },
   { timestamps: true }
 );
 
-// Hash password before saving
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error);
-  }
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
+  this.password = await bcrypt.hash(this.password, 10);
 });
 
-// Method to compare passwords
 userSchema.methods.comparePassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
